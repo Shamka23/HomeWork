baf4a1211bed9d5fa4aedf5078f86171303601ae
ORION PAX <www.morta1325@gmail.com>
2025-10-12 16:54:56 +0300

    Добавил решение задачи на нахождение наибольшего из преданных чисел

--- src/main/java/org/example/numvar/Main.java
+++ src/main/java/org/example/numvar/Main.java
@@ -7,16 +7,29 @@ public class Main {
 
         System.out.println(isEven(4));
         System.out.println(isEven(7));
+
+        System.out.println(max(9, 28, 1));
     }
 
     private static void sum(int firstCount, int secondCount) {
         System.out.println(firstCount + secondCount);
     }
 
-    private static boolean isEven(int equivalent){
+    private static boolean isEven(int equivalent) {
         if (equivalent % 2 == 1) {
             return false;
         }
         return true;
+    }
+
+    private static int max(int firstCount, int secondCount, int thirdCount) {
+        if (firstCount > secondCount && firstCount > thirdCount) {
+            return firstCount;
+        } else if (secondCount > firstCount && secondCount > thirdCount) {
+            return secondCount;
+        } else if (thirdCount > firstCount && thirdCount > secondCount) {
+            return thirdCount;
+        }
+        return 0;
     }
 }
