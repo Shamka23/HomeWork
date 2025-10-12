59e1643f0f54e06e32d3d8b1b0316d169e36d164
ORION PAX <www.morta1325@gmail.com>
2025-10-12 21:11:14 +0300

    Исправил названия методов и имена параметров метода

--- src/main/java/org/example/cicles/Main.java
+++ src/main/java/org/example/cicles/Main.java
@@ -2,28 +2,30 @@ package org.example.cicles;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sumToN(0));
-        System.out.println(sumToN(5));
+        System.out.println(sumNumbersFromOneToOur(0));
+        System.out.println(sumNumbersFromOneToOur(5));
         System.out.println();
 
-        System.out.println(mulRow(3));
-        System.out.println(mulRow(9));
+        System.out.println(multiplicationRow(3));
+        System.out.println(multiplicationRow(9));
     }
 
-    private static long sumToN(int n) {
+    private static long sumNumbersFromOneToOur(int number) {
         int sum = 0;
-        while (n >= 0) {
-            sum = sum + n;
-            n--;
+        while (number >= 0) {
+            sum = sum + number;
+            number--;
         }
         return sum;
     }
 
-    private static String mulRow(int n){
+    private static String multiplicationRow(int number){
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= n; i++) {
-            sb.append(n).append(" * ").append(i).append(" = ").append(n * i).append("\n");
+        for (int i = 1; i <= number; i++) {
+            sb.append(number).append(" * ").append(i).append(" = ").append(number * i).append("\n");
         }
         return sb.toString();
     }
+
+
 }
