36577cad026ee6197267ddac1ff7784a676c2b78
ORION PAX <www.morta1325@gmail.com>
2025-10-12 20:56:53 +0300

    Поправил метод sign. Поменял имя метода на более понятное, а также учёл предыдущие оибки и изменил имя переменной, которая поступила в параметрах метода

--- src/main/java/org/example/condition/Main.java
+++ src/main/java/org/example/condition/Main.java
@@ -2,9 +2,9 @@ package org.example.condition;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sign(-10));
-        System.out.println(sign(0));
-        System.out.println(sign(10));
+        System.out.println(getSign(-10));
+        System.out.println(getSign(0));
+        System.out.println(getSign(10));
         System.out.println();
 
         System.out.println(isLeap(2000));
@@ -15,10 +15,10 @@ public class Main {
         System.out.println(ticketPrice(80));
     }
 
-    private static String sign(int n) {
-        if (n == 0) {
+    private static String getSign(int number) {
+        if (number == 0) {
             return "zero";
-        } else if (n < 0) {
+        } else if (number < 0) {
             return "negative";
         } else {
             return "positive";
